60a0fcf03f1f999cf7a250a7ebf2781e74b218e0
Le Hai Ninh <ninh@felizz.com>
2017-04-09 21:16:45 +0700

    Removed un-neccessary parts to deploy

--- views/react/Landing/LandingHeader.jsx
+++ views/react/Landing/LandingHeader.jsx
@@ -6,14 +6,6 @@ class LandingHeader extends React.Component {
     render() {
         return (
             <div>
-                <ul styleName="menu">
-                    <li styleName="menu_item">
-                        <a href="/travel">Travel</a>
-                    </li>
-                    <li styleName="menu_item">Programming</li>
-                    <li styleName="menu_item">Others</li>
-                </ul>
-                <div styleName="clearfix"></div>
             </div>
         );
     }

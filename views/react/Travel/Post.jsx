8a0e80a70ccd57350ee4defd965dcf8397162b48
Le Hai Ninh <ninh@felizz.com>
2017-04-09 16:38:38 +0700

    Added meta tag for posts

--- views/react/Travel/Post.jsx
+++ views/react/Travel/Post.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styles from './post.scss';
 import CSSModules from 'react-css-modules';
+import PropTypes from 'prop-types';
+
+let convertTimestampToReadable = (date) => {
+    let date_string = date.getDate();
+    let month_string = date.getMonth();
+    if (date_string  < 10) date_string = '0' + date_string;
+    if (month_string < 10) month_string = '0' + month_string;
+    return `${date_string}-${month_string}-${date.getFullYear()}`;
+};
 
 class Post extends React.Component {
     render() {
         let post = this.props.post;
         let post_url = '/';
+        let created_at = convertTimestampToReadable(post.created_at);
         return (
             <div styleName="post_container">
                 <div styleName="post">
                     <a styleName="post_title" href={post_url}>
                         {post.title}
                     </a>
+                    <div styleName="post_stamp">
+                        <p>
+                            <span styleName="date">{created_at} </span>
+                            <span style={{"font-style": "italic"}}>in </span>
+                            <a href='/' styleName="post_tag">
+                                {post.tag}
+                            </a>
+                        </p>
+                    </div>
                     {
                         post.photo && (
                             <img src={post.photo}/>
@@ -25,7 +44,7 @@ class Post extends React.Component {
 }
 
 Post.propTypes = {
-    post: React.PropTypes.object.isRequired
+    post: PropTypes.object.isRequired
 };
 
 export default CSSModules(Post, styles, {allowMultiple: true});

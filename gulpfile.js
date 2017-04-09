47dac9ad77d541181b62b3aac15edaf19121a85b
Le Hai Ninh <ninh@felizz.com>
2017-04-09 21:56:22 +0700

    Build for production

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ let nodemon = require('gulp-nodemon');
 let webpack = require('webpack');
 let webpack_config = require('./webpack.config.js');
 
-gulp.task('jsx:watch', (done) => {
+gulp.task('webpack:watch', (done) => {
     let firedDone = false;
     webpack(webpack_config).watch({
         aggregateTimeout: 1000,
@@ -24,9 +24,37 @@ gulp.task('jsx:watch', (done) => {
     });
 });
 
-gulp.task('default', ['jsx:watch'], () => {
+gulp.task('webpack:build', (done) => {
+    webpack_config.plugins = webpack_config.plugins.concat([
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false,
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
+    ]);
+    webpack(webpack_config).run((err, res) => {
+        if (err) {
+            console.error('ERROR: ', err, {file: __filename});
+        } else {
+            console.info('DONE: [webpack hash]', res.hash, ' TIME (ms): ', new Date(res.endTime - res.startTime).getTime(), {file: __filename});
+        }
+        done();
+    });
+});
+
+gulp.task('default', ['webpack:watch'], () => {
     nodemon({
         script: 'app.js',
         ignore: 'public/*'
     });
+});
+
+gulp.task('production', ['webpack:build'], () => {
+
 });

07d915f501d31e9ebff5da710a1e4e1ada51508a
Le Hai Ninh <ninh@felizz.com>
2017-04-22 22:12:42 +0700

    feat: added favicon.ico

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require('express');
+let favicon = require('serve-favicon');
+let path = require('path');
 let app = express();
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'resources/img/favicon.ico')));
 app.get('/', (req, res) => {
     res.render('landing', {title: 'Le Hai Ninh'});
 });
